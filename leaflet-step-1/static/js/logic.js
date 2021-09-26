fd6db81d28832a8f3a6eaae01c603650c7705a34
Karen Gutierrez <kareng018@berkeley.edu>
2021-09-26 11:02:17 -0700

    map rendering

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -13,12 +13,16 @@ var myMap = L.map("mapid", {
   // Add a tile layer 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-    tileSize: 512,
+    tileSize: 850,
     maxZoom: 18,
     zoomOffset: -1,
     id: 'mapbox/light-v10',
     accessToken: API_KEY
 }).addTo(myMap);
+
+$('#modalID').on('shown.bs.modal', function() {
+    myMap.invalidateSize();
+  });
 
 // D3 retrieval
 d3.json(url, function(data){
